7950d7100d14d50c8de2acd3a459aa24abe7a02e
Derk Norton <derk.norton@gmail.com>
2022-12-15 14:45:52 -0700

    Done testing the panic statements where possible.

--- sorter.go
+++ sorter.go
@@ -91,6 +91,7 @@ func (v *sorter[V]) shuffleArray(array []V) {
 func randomIndex(size int) int {
 	var random, err = ran.Int(ran.Reader, big.NewInt(int64(size)))
 	if err != nil {
+		// There was an issue with the underlying OS so time to...
 		panic("Unable to generate a random index:\n" + err.Error())
 	}
 	return int(random.Int64())

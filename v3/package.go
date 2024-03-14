9607da1a1a24dab9e313880112a1d4ee6a11af7c
Derk Norton <derk.norton@gmail.com>
2024-03-14 16:04:38 -0600

    Reformatted the copyright statements to work better with gofmt.

--- v3/package.go
+++ v3/package.go
@@ -1,12 +1,14 @@
-/*******************************************************************************
- *   Copyright (c) 2009-2024 Crater Dog Technologies™.  All Rights Reserved.   *
- *******************************************************************************
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               *
- *                                                                             *
- * This code is free software; you can redistribute it and/or modify it under  *
- * the terms of The MIT License (MIT), as published by the Open Source         *
- * Initiative. (See http://opensource.org/licenses/MIT)                        *
- *******************************************************************************/
+/*
+................................................................................
+.    Copyright (c) 2009-2024 Crater Dog Technologies™.  All Rights Reserved.   .
+................................................................................
+.  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.               .
+.                                                                              .
+.  This code is free software; you can redistribute it and/or modify it under  .
+.  the terms of The MIT License (MIT), as published by the Open Source         .
+.  Initiative. (See http://opensource.org/licenses/MIT)                        .
+................................................................................
+*/
 
 /*
 Package collections defines a set of simple, pragmatic abstract types and

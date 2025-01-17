5b212e76a2a30ade51e7ca498c9f27d32fe3182e
Derk Norton <derk.norton@gmail.com>
2025-01-17 08:29:23 -0700

    Updated the example code tests.

--- v5/Module_test.go
+++ v5/Module_test.go
@@ -128,40 +128,48 @@ func TestModuleExampleCode(t *tes.T) {
 func TestListExampleCode(t *tes.T) {
 	fmt.Println("LIST EXAMPLE:")
 
-	// Create a new list using the universal constructor.
+	// Create a new list from an array.
 	var list = fra.ListFromArray[string](
 		[]string{"bar", "foo", "bax"},
 	)
 	fmt.Println("The initialized list:", list)
+	fmt.Println()
 
 	// Add some more values to the list.
 	list.AppendValue("fuz")
 	list.AppendValue("box")
 	fmt.Println("The augmented list:", list)
+	fmt.Println()
 
 	// Change a value in the list.
 	list.SetValue(2, "foz")
 	fmt.Println("The updated list:", list)
+	fmt.Println()
 
 	// Insert a new value at the beginning of the list (slot 0).
 	list.InsertValue(0, "bax")
 	fmt.Println("The updated list:", list)
+	fmt.Println()
 
 	// Insert a new value at the end of the list (slot N).
 	list.InsertValue(6, "bux")
 	fmt.Println("The updated list:", list)
+	fmt.Println()
 
 	// Sort the values in the list.
 	list.SortValues()
 	fmt.Println("The sorted list:", list)
+	fmt.Println()
 
 	// Randomly shuffle the values in the list.
 	list.ShuffleValues()
 	fmt.Println("The shuffled list:", list)
+	fmt.Println()
 
 	// Remove a value from the list.
 	list.RemoveValue(4)
 	fmt.Println("The shortened list:", list)
+	fmt.Println()
 
 	// Remove all values from the list.
 	list.RemoveAll()
@@ -172,28 +180,34 @@ func TestListExampleCode(t *tes.T) {
 func TestSetExampleCode(t *tes.T) {
 	fmt.Println("SET EXAMPLE:")
 
-	// Create two sets with overlapping values using the universal constructor.
+	// Create two sets with overlapping values.
 	var set1 = fra.SetFromArray[string](
 		[]string{"alpha", "beta", "gamma"},
 	)
 	fmt.Println("The first set is:", set1)
+	fmt.Println()
 	var set2 = fra.SetFromArray[string](
 		[]string{"beta", "gamma", "delta"},
 	)
 	fmt.Println("The second set is:", set2)
+	fmt.Println()
 
 	// Find the logical union of the two sets.
 	var Set = set1.GetClass()
 	fmt.Println("The union of the two sets is:", Set.Or(set1, set2))
+	fmt.Println()
 
 	// Find the logical difference between the two sets.
 	fmt.Println("The first set minus the second set is:", Set.Sans(set1, set2))
+	fmt.Println()
 
 	// Find the logical intersection of the two sets.
 	fmt.Println("The intersection of the two sets is:", Set.And(set1, set2))
+	fmt.Println()
 
 	// Find the logical exclusion of the two sets.
 	fmt.Println("The exclusion of the two sets is:", Set.Xor(set1, set2))
+	fmt.Println()
 
 	// Add an existing value to the first set.
 	set1.AddValue("beta")
@@ -204,26 +218,33 @@ func TestSetExampleCode(t *tes.T) {
 func TestStackExampleCode(t *tes.T) {
 	fmt.Println("STACK EXAMPLE:")
 
-	// Create a new empty stack using the universal constructor.
+	// Create a new empty stack.
 	var stack = fra.Stack[string]()
 	fmt.Println("The empty stack:", stack)
+	fmt.Println()
 
 	// Add some values to it.
 	stack.AddValue("foo")
 	fmt.Println("The stack with one value on it:", stack)
+	fmt.Println()
 	stack.AddValue("bar")
 	fmt.Println("The stack with two values on it:", stack)
+	fmt.Println()
 	stack.AddValue("baz")
 	fmt.Println("The stack with three values on it:", stack)
+	fmt.Println()
 
 	// Remove the top value from the stack.
 	var top = stack.RemoveLast()
 	fmt.Println("The top value was:", top)
+	fmt.Println()
 	fmt.Println("The stack with only two values on it:", stack)
+	fmt.Println()
 
 	// Remove all values from the stack.
 	stack.RemoveAll()
 	fmt.Println("The stack with no more values on it:", stack)
+	fmt.Println()
 	var isEmpty = stack.IsEmpty()
 	fmt.Println("The stack is now empty?", isEmpty)
 	fmt.Println()
@@ -236,16 +257,19 @@ func TestQueueExampleCode(t *tes.T) {
 	var wg = new(syn.WaitGroup)
 	defer wg.Wait()
 
-	// Create a new queue with a specific capacity using the universal constructor.
+	// Create a new queue with a specific capacity.
 	var queue = fra.QueueWithCapacity[int](12)
 	fmt.Println("The empty queue:", queue)
+	fmt.Println()
 
 	// Add some values to the queue.
 	for i := 1; i < 10; i++ {
 		queue.AddValue(i)
 		fmt.Println("Added value:", i)
 	}
+	fmt.Println()
 	fmt.Println("The partially filled queue:", queue)
+	fmt.Println()
 
 	// Remove values from the queue in the background.
 	wg.Add(1)
@@ -274,7 +298,7 @@ func TestQueueExampleCode(t *tes.T) {
 func TestCatalogExampleCode(t *tes.T) {
 	fmt.Println("CATALOG EXAMPLE:")
 
-	// Create a new catalog using the universal constructor.
+	// Create a new catalog from a map.
 	var catalog = fra.CatalogFromMap[string, int64](
 		map[string]int64{
 			"foo": 1,
@@ -283,29 +307,36 @@ func TestCatalogExampleCode(t *tes.T) {
 		},
 	)
 	fmt.Println("The initialized catalog:", catalog)
+	fmt.Println()
 
 	// Add a new association to the catalog.
 	catalog.SetValue("fuz", 4)
 	fmt.Println("The updated catalog:", catalog)
+	fmt.Println()
 
 	// Sort the associations in the catalog by key.
 	catalog.SortValues()
 	fmt.Println("The sorted catalog:", catalog)
+	fmt.Println()
 
 	// List the keys for the catalog.
 	var keys = catalog.GetKeys()
 	fmt.Println("The keys for the catalog:", keys)
+	fmt.Println()
 
 	catalog.ReverseValues()
 	fmt.Println("The reversed catalog:", catalog)
+	fmt.Println()
 
 	// Retrieve a value from the catalog.
 	var value = catalog.GetValue("bar")
 	fmt.Println("The value for the \"bar\" key is", value)
+	fmt.Println()
 
 	// Remove a value from the catalog.
 	catalog.RemoveValue("foo")
 	fmt.Println("The smaller catalog:", catalog)
+	fmt.Println()
 
 	// Change an existing value in the catalog.
 	catalog.SetValue("baz", 5)
@@ -313,10 +344,92 @@ func TestCatalogExampleCode(t *tes.T) {
 	fmt.Println()
 }
 
+func TestCollatorExampleCode(t *tes.T) {
+	fmt.Println("COLLATOR EXAMPLE:")
+
+	// Create a collator with the default maximum depth.
+	var collator = fra.Collator[any]()
+
+	// Collate two strings.
+	var s1 = "first"
+	var s2 = "second"
+	fmt.Println(
+		"The first and second strings are equal:",
+		collator.CompareValues(s1, s2),
+	)
+	fmt.Println(
+		"The first string is ranked before the second strings:",
+		collator.RankValues(s1, s2) == fra.LesserRank,
+	)
+	fmt.Println()
+
+	// Collate two arrays.
+	var a1 = []int{1, 2, 3}
+	var a2 = []int{1, 3, 2}
+	fmt.Println(
+		"The first and second arrays are equal:",
+		collator.CompareValues(a1, a2),
+	)
+	fmt.Println(
+		"The first array is ranked before the second array:",
+		collator.RankValues(a1, a2) == fra.LesserRank,
+	)
+	fmt.Println()
+}
+
+func TestSorterExampleCode(t *tes.T) {
+	fmt.Println("SORTER EXAMPLE:")
+
+	// Create a sorter with the default (natural) ranker.
+	var sorter = fra.Sorter[string]()
+
+	// Create an array.
+	var array = []string{
+		"alpha",
+		"beta",
+		"gamma",
+		"delta",
+	}
+	fmt.Println("The initial ordering of the values:", array)
+	fmt.Println()
+
+	// Sort the values in alphabetical order.
+	sorter.SortValues(array)
+	fmt.Println("The values in alphabetical order:", array)
+	fmt.Println()
+
+	// Sort the values in reverse order.
+	sorter.ReverseValues(array)
+	fmt.Println("The values in reverse order:", array)
+	fmt.Println()
+
+	// Shuffle the order of the values.
+	sorter.ShuffleValues(array)
+	fmt.Println("The values in random order:", array)
+	fmt.Println()
+
+	// Sort the values with a custom ranking function.
+	sorter = fra.SorterWithRanker[string](
+		func(first, second string) fra.Rank {
+			switch {
+			case first < second:
+				return fra.GreaterRank
+			case first > second:
+				return fra.LesserRank
+			default:
+				return fra.EqualRank
+			}
+		},
+	)
+	sorter.SortValues(array)
+	fmt.Println("The values in custom order:", array)
+	fmt.Println()
+}
+
 func TestIteratorExampleCode(t *tes.T) {
 	fmt.Println("ITERATOR EXAMPLE:")
 
-	// Create a list using the universal constructor and an iterator for it.
+	// Create a list from an array.
 	var list = fra.ListFromArray[string](
 		[]string{"foo", "bar", "baz"},
 	)
@@ -328,11 +441,13 @@ func TestIteratorExampleCode(t *tes.T) {
 		var value = iterator.GetNext()
 		fmt.Println("    value:", value)
 	}
+	fmt.Println()
 
 	// Go to a specific value in the list.
 	iterator.SetSlot(2)
 	var value = iterator.GetPrevious()
 	fmt.Println("The second value in the list is:", value)
+	fmt.Println()
 
 	// Iterate over the values in reverse order.
 	fmt.Println("The list values in reverse order:")
